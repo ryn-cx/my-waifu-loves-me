4e40355735617e4d1083a8e220c1d5e1895e6ac9
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2025-07-05 20:55:20 -0700

    🚀 Added automatic API route loading

--- backend/app/api_router.py
+++ backend/app/api_router.py
@@ -1,18 +1,38 @@
+from importlib import import_module
+from pathlib import Path
+
 from fastapi import APIRouter
 
 from app.core.config import settings
-from app.items.router import router as items_router
-from app.login.router import router as login_router
-from app.private.router import router as private_router
-from app.users.router import router as users_router
-from app.utils.router import router as utils_router
 
 api_router = APIRouter()
-api_router.include_router(items_router)
-api_router.include_router(login_router)
-api_router.include_router(users_router)
-api_router.include_router(utils_router)
+
+# Automatically load all of the routers from app/*/router.py
+app_folder = Path(__file__).parent
+for model_files in app_folder.glob("*/router.py"):
+    module_name = model_files.parent.name
+    router = import_module(f"app.{module_name}.router").router
+
+    if module_name == "private":
+        if settings.ENVIRONMENT == "local":
+            api_router.include_router(router)
+    else:
+        api_router.include_router(router)
+
+# # Alternative method to manually load all of the routers from
+# from app.core.config import settings
+# from app.items.router import router as items_router
+# from app.login.router import router as login_router
+# from app.private.router import router as private_router
+# from app.users.router import router as users_router
+# from app.utils.router import router as utils_router
+
+# api_router = APIRouter()
+# api_router.include_router(items_router)
+# api_router.include_router(login_router)
+# api_router.include_router(users_router)
+# api_router.include_router(utils_router)
 
 
-if settings.ENVIRONMENT == "local":
-    api_router.include_router(private_router)
+# if settings.ENVIRONMENT == "local":
+#     api_router.include_router(private_router)
