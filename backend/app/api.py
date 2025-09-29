93afeaa88a52a84c2e6954681902348ddf536846
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2025-09-29 04:02:46 -0700

    Removed unused commented out code

--- backend/app/api.py
+++ backend/app/api.py
@@ -18,21 +18,3 @@ for model_files in app_folder.glob("*/router.py"):
             api_router.include_router(router)
     else:
         api_router.include_router(router)
-
-# # Alternative method to manually load all of the routers
-# from app.config import settings
-# from app.items.router import router as items_router
-# from app.login.router import router as login_router
-# from app.private.router import router as private_router
-# from app.users.router import router as users_router
-# from app.utils.router import router as utils_router
-
-# api_router = APIRouter()
-# api_router.include_router(items_router)
-# api_router.include_router(login_router)
-# api_router.include_router(users_router)
-# api_router.include_router(utils_router)
-
-
-# if settings.ENVIRONMENT == "local":
-#     api_router.include_router(private_router)
